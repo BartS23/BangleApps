4322e05b88d559ca9a3103cd04396276ce34a8e1
BartS23 <10829389+BartS23@users.noreply.github.com>
2022-04-03 21:22:19 +0200

    Simplify Clock

--- apps/rmkClock/app.js
+++ apps/rmkClock/app.js
@@ -1,7 +1,3 @@
-const appId = "RMKApp";
-const screenHeight = g.getHeight();
-const screenWidth = g.getWidth();
-
 const locale = require("locale");
 const Layout = require("Layout");
 
@@ -9,7 +5,6 @@ const Layout = require("Layout");
 require("Font7x11Numeric7Seg").add(Graphics);
 
 function layoutRedraw(layout, element, newValue) {
-  //console.log("layoutRedraw");
   if (layout[element].label !== newValue) {
     layout.clear(layout[element]);
     layout[element].label = newValue;
@@ -17,207 +12,88 @@ function layoutRedraw(layout, element, newValue) {
   }
 }
 
-function draw() {
-  //console.log("draw");
-  g.reset();
-
-  if (screens[currentScreen]) {
-    g.setFont("Vector", screenWidth / 7);
-    g.setFontAlign(0, 0);
-    g.setColor(screens[currentScreen].bgColor);
-    g.fillRect(0, 24, screenWidth - 1, screenHeight - 1);
-    g.setColor(screens[currentScreen].color);
-    g.drawString((screens[currentScreen].name), screenWidth / 2, (3 * (screenHeight - 24) / 4) + 24);
-  }
-}
-
-const screens = [{
-  name: "Clock",
-  activated: false,
-  draw: function () {
-    //console.log("clock draw");
-    var d = new Date();
-
-    var h = d.getHours().toString();
-    var m = d.getMinutes().toString();
-    var dow = locale.dow(d, 1);
-    var day = d.getDate().toString();
-    var month = (d.getMonth() + 1).toString();
-    var year = d.getFullYear().toString().substr(-2);
-
-    var time = h.padStart(2, "0") + ":" + m.padStart(2, "0");
-    var dateStr = `${dow} ${day.padStart(2, "0")}.${month.padStart(2, "0")}.${year}`;
-
-    layoutRedraw(this.layout, "time", time);
-    layoutRedraw(this.layout, "date", dateStr);
-    layoutRedraw(this.layout, "steps", locale.number(Bangle.getHealthStatus("day").steps, 0));
-
-    this.clockTimeout = setTimeout(this.draw.bind(this), 60000 - (Date.now() % 60000));
+const queueMillis = 60000;
+const layout = new Layout({
+  type: "v",
+  c: [{
+    type: "txt",
+    font: "7x11Numeric7Seg:4",
+    label: "     ",
+    id: "time"
   },
-  onActivate: function () {
-    //console.log("clock onActivate");
-    g.reset();
-    g.clearRect(0, 24, screenWidth, screenHeight);
-
-    this.draw();
+  { height: 2 },
+  {
+    type: "txt",
+    font: "15%",
+    label: "",
+    id: "date",
+    fillx: 1
   },
-  onDeactivate: function () {
-    //console.log("clock onDeactivate");
-    if (this.clockTimeout) {
-      clearTimeout(this.clockTimeout);
-    }
-    if (this.stepsTimeout) {
-      clearTimeout(this.stepsTimeout);
-    }
-
-    this.clockTimeout = this.stepsTimeout = undefined;
-  },
-  layout: new Layout({
-    type: "v",
+  { height: 5 },
+  {
+    type: "h",
+    halign: -1,
     c: [{
       type: "txt",
-      font: "7x11Numeric7Seg:4",
-      label: "     ",
-      id: "time"
+      font: "10%",
+      col: "#0f0",
+      label: "Steps: ",
+      id: "stepsText"
     },
-    {
-      height: 2
-    },
+    { width: 5 },
     {
       type: "txt",
-      font: "15%",
-      label: "",
-      id: "date",
-      fillx: 1
-    },
-    {
-      height: 5
-    },
-    {
-      type: "h",
+      font: "10%",
       halign: -1,
-      c: [{
-        type: "txt",
-        font: "10%",
-        col: "#0f0",
-        label: "Steps: ",
-        id: "stepsText"
-      },
-      {
-        width: 5
-      },
-      {
-        type: "txt",
-        font: "10%",
-        halign: -1,
-        col: "#0f0",
-        id: "steps",
-        label: "0",
-        fillx: 1
-      }
-      ]
+      col: "#0f0",
+      id: "steps",
+      label: "0",
+      fillx: 1
     }
-    ],
-    lazy: true
-  })
-},
-{
-  name: "GPS",
-  activated: false,
-  onActivate: () => {
-    //console.log("gps onactiv");
-    /*            E.showPrompt("GPS " + locale.translate("on") + "?", {
-                    title: "GPS " + locale.translate("on") + "?",
-                    buttons: { yes: true, no: false }
-                });*/
+    ]
   }
-},
-{
-  name: "Test3",
-  bgColor: "#0f0",
-  color: "#fff"
-},
-{
-  name: "Test4",
-  bgColor: "#ff0",
-  color: "#fff"
-},
-{
-  name: "Test5",
-  bgColor: "#00f",
-  color: "#fff"
-},
-{
-  name: "Test6",
-  bgColor: "#f0f",
-  color: "#fff"
-},
-{
-  name: "Test7",
-  bgColor: "#0ff",
-  color: "#fff"
-},
-{
-  name: "Test8",
-  bgColor: "#fff",
-  color: "#000"
-},
-];
+  ],
+  lazy: true
+});
 
-var currentScreen = 0;
-
-function changeScreen(dir) {
-  //console.log("Swipe");
-  if (currentScreen > -1) {
-    if (screens[currentScreen].onDeactivate) {
-      screens[currentScreen].onDeactivate();
-      screens[currentScreen].activated = false;
-    }
-
-    currentScreen += dir;
-    if (currentScreen < 0) {
-      currentScreen = screens.length - 1;
-    } else if (currentScreen === screens.length) {
-      currentScreen = 0;
-    }
-
-    if (screens[currentScreen].onActivate) {
-      //console.log(`onActivate ${currentScreen}`);
-      screens[currentScreen].onActivate();
-      screens[currentScreen].activated = true;
-    } else {
-      draw();
-    }
-
-    if (screens[currentScreen].layout) {
-      //console.log(`Layout render ${currentScreen}`);
-      screens[currentScreen].layout.render();
-    }
-  }
+var drawTimeout;
+function queueDraw() {
+  if (drawTimeout) clearTimeout(drawTimeout);
+  drawTimeout = setTimeout(function () {
+    drawTimeout = undefined;
+    draw();
+  }, queueMillis - (Date.now() % queueMillis));
 }
 
-Bangle.on('swipe', changeScreen);
+function draw() {
+  var d = new Date();
 
-Bangle.on(' ', on => {
-  //console.log("lcdPower");
-  if (on && !screens[currentScreen].activated) {
-    if (screens[currentScreen].onActivate) {
-      screens[currentScreen].onActivate();
-    } else {
-      draw();
-    }
-  } else if (!on && screens[currentScreen].activated) {
-    if (screens[currentScreen].onDeactivate) {
-      screens[currentScreen].onDeactivate();
-    }
-  }
-});
+  var h = d.getHours().toString();
+  var m = d.getMinutes().toString();
+  var dow = locale.dow(d, 1);
+  var day = d.getDate().toString();
+  var month = (d.getMonth() + 1).toString();
+  var year = d.getFullYear().toString().slice(-2);
+
+  var time = h.padStart(2, "0") + ":" + m.padStart(2, "0");
+  var dateStr = `${dow} ${day.padStart(2, "0")}.${month.padStart(2, "0")}.${year}`;
+
+  layoutRedraw(layout, "time", time);
+  layoutRedraw(layout, "date", dateStr);
+  layoutRedraw(layout, "steps", locale.number(Bangle.getHealthStatus("day").steps, 0));
+
+  layout.render();
+
+  queueDraw();
+}
+
+// Clear the screen once, at startup
 g.clear();
+
+draw();
 
 // Show launcher when button pressed
 Bangle.setUI("clock");
-
-changeScreen(0);
 
 Bangle.loadWidgets();
 Bangle.drawWidgets();
