b0cdb4e7700f5f95abe614366f22487daea15de8
BartS23 <10829389+BartS23@users.noreply.github.com>
2022-10-27 15:59:57 +0200

    switch to ClockFace

--- apps/rmkClock/app.js
+++ apps/rmkClock/app.js
@@ -1,99 +1,76 @@
-const locale = require("locale");
-const Layout = require("Layout");
+const ClockFace = require("ClockFace");
+let clock = new ClockFace({
+  init: function () {
+    require("Font7x11Numeric7Seg").add(Graphics);
+    this.layoutRedraw = (layout, element, newValue) => {
+      if (layout[element].label !== newValue) {
+        layout.clear(layout[element]);
+        layout[element].label = newValue;
+        layout.render(layout[element]);
+      }
+    };
+    this.locale = require("locale");
 
-// Load fonts
-require("Font7x11Numeric7Seg").add(Graphics);
-
-function layoutRedraw(layout, element, newValue) {
-  if (layout[element].label !== newValue) {
-    layout.clear(layout[element]);
-    layout[element].label = newValue;
-    layout.render(layout[element]);
-  }
-}
-
-const queueMillis = 60000;
-const layout = new Layout({
-  type: "v",
-  c: [{
-    type: "txt",
-    font: "7x11Numeric7Seg:4",
-    label: "     ",
-    id: "time"
+    const Layout = require("Layout");
+    this.layout = new Layout({
+      type: "v",
+      c: [{
+        type: "txt",
+        font: "7x11Numeric7Seg:4",
+        label: "     ",
+        id: "time"
+      }, 
+      { height: 2 }, 
+      {
+        type: "txt",
+        font: "15%",
+        label: "",
+        id: "date",
+        fillx: 1
+      }, 
+      { height: 5 }, 
+      {
+        type: "h",
+        halign: -1,
+        c: [{
+          type: "txt",
+          font: "10%",
+          col: "#0f0",
+          label: "Steps: ",
+          id: "stepsText"
+        }, 
+        { width: 5 }, 
+        {
+          type: "txt",
+          font: "10%",
+          halign: -1,
+          col: "#0f0",
+          id: "steps",
+          label: "0",
+          fillx: 1
+        }]
+      }],
+      lazy: true
+    });
   },
-  { height: 2 },
-  {
-    type: "txt",
-    font: "15%",
-    label: "",
-    id: "date",
-    fillx: 1
-  },
-  { height: 5 },
-  {
-    type: "h",
-    halign: -1,
-    c: [{
-      type: "txt",
-      font: "10%",
-      col: "#0f0",
-      label: "Steps: ",
-      id: "stepsText"
-    },
-    { width: 5 },
-    {
-      type: "txt",
-      font: "10%",
-      halign: -1,
-      col: "#0f0",
-      id: "steps",
-      label: "0",
-      fillx: 1
+  update: function (time, changed) {
+    if (changed.d) {
+      var dow = this.locale.dow(time, 1);
+      var day = time.getDate().toString();
+      var month = (time.getMonth() + 1).toString();
+      var year = time.getFullYear().toString().slice(-2);
+      var dateStr = `${dow} ${day.padStart(2, "0")}.${month.padStart(2, "0")}.${year}`;
+      this.layoutRedraw(this.layout, "date", dateStr);
     }
-    ]
-  }
-  ],
-  lazy: true
+    if (changed.h || changed.m) {
+      var h = time.getHours().toString();
+      var m = time.getMinutes().toString();
+      var timeText = h.padStart(2, "0") + ":" + m.padStart(2, "0");
+      this.layoutRedraw(this.layout, "time", timeText);
+      this.layout.render(this.layout.stepsText);
+      this.layoutRedraw(this.layout, "steps", this.locale.number(Bangle.getHealthStatus("day").steps, 0));
+    }
+  },
 });
 
-var drawTimeout;
-function queueDraw() {
-  if (drawTimeout) clearTimeout(drawTimeout);
-  drawTimeout = setTimeout(function () {
-    drawTimeout = undefined;
-    draw();
-  }, queueMillis - (Date.now() % queueMillis));
-}
-
-function draw() {
-  var d = new Date();
-
-  var h = d.getHours().toString();
-  var m = d.getMinutes().toString();
-  var dow = locale.dow(d, 1);
-  var day = d.getDate().toString();
-  var month = (d.getMonth() + 1).toString();
-  var year = d.getFullYear().toString().slice(-2);
-
-  var time = h.padStart(2, "0") + ":" + m.padStart(2, "0");
-  var dateStr = `${dow} ${day.padStart(2, "0")}.${month.padStart(2, "0")}.${year}`;
-
-  layoutRedraw(layout, "time", time);
-  layoutRedraw(layout, "date", dateStr);
-  layoutRedraw(layout, "steps", locale.number(Bangle.getHealthStatus("day").steps, 0));
-
-  layout.render();
-
-  queueDraw();
-}
-
-// Clear the screen once, at startup
-g.clear();
-
-draw();
-
-// Show launcher when button pressed
-Bangle.setUI("clock");
-
-Bangle.loadWidgets();
-Bangle.drawWidgets();
+clock.start();
