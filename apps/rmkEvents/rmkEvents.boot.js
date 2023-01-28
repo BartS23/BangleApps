acefce3bd1c0c636c137575c7889691020931d19
BartS23 <10829389+BartS23@users.noreply.github.com>
2023-01-28 17:14:58 +0100

    cleanup on midnight

--- apps/rmkEvents/rmkEvents.boot.js
+++ apps/rmkEvents/rmkEvents.boot.js
@@ -1,2 +1,12 @@
 
 setTimeout(() => require("rmkEvents").update());
+Bangle.on('midnight', () => {
+  const storage = require("Storage");
+  const logFiles = storage.list(/^rmkEvents-.*\.log$/, { sf: true });
+  let date = new Date();
+  date.setDate(date.getDate() - 2);
+  date = (date.toJSON().split("T"))[0];
+  logFiles
+    .filter(file => /^rmkEvents-([\d-]+)/.exec(file)[1] <= date)
+    .forEach(file => storage.open(file, 'r').erase());
+});
